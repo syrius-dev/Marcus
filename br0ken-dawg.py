ede76a3a14830c95ef62f37c97cc7b14d3fd95f5
syrius-dev <32462830+syrius-dev@users.noreply.github.com>
2017-10-02 13:19:19 -0400

    Create br0ken-dawg.py

---
+++ br0ken-dawg.py
@@ -0,0 +1,33 @@
+# Python script written by syrius
+# Proof of concept that Marcus is morally and spiritually a broken dog
+print("Introducing Marcus, a small neurotic retarded dog")
+print("""\
+          __
+ \ ______/ V`-,
+  }        /~~
+ /_)^ --,r'
+|b      |b
+""")
+print ("What\'s your name ?")
+name = raw_input("--> ")
+print ("Which exercise would you like to work with Marcus today, %s?") % name
+print ("1- Sit")
+print ("2- Give your paw")
+print ("3- Kill yourself")
+exercise = raw_input("--> ")
+if exercise == "1":
+    print("""\
+       _
+      _V.-o
+     / |`-'
+    (7_\\
+    """)
+    print ("Good dog!")
+if exercise == "2":
+    print("""\
+    ╭∩╮（︶︿︶）╭∩╮
+    """)
+    print("-------------------------")
+    print("O RLY!? You ungrateful dog!")
+else:
+    print("Not available now.")
