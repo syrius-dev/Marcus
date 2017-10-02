63840589c522532c90b7f6a9a69fbe8b2193568a
syrius-dev <32462830+syrius-dev@users.noreply.github.com>
2017-10-02 16:12:57 -0400

    Update br0ken-dawg.py

--- br0ken-dawg.py
+++ br0ken-dawg.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# The line above has been added to show ascii art properly
 # Python script written by syrius
 # Proof of concept that Marcus is morally and spiritually a broken dog
 # Contact: code2lulz@protonmail.com
@@ -25,11 +27,34 @@ if exercise == "1":
     (7_\\
     """)
     print ("Good dog!")
-if exercise == "2":
+elif exercise == "2":
     print("""\
     ╭∩╮（︶︿︶）╭∩╮
     """)
     print("-------------------------")
     print("O RLY!? You ungrateful dog!")
+    print("Please choose the punishment to give to Marcus")
+    print("1- Kick his a$s")
+    print("2- Do nothing")
+    punishment = raw_input("--> ")
+    if punishment == "1":
+        print("""
+         aie aie aie              
+                ''',
+        o_)O \)____)"
+         \_        )
+           '',,,,,,
+             ||  ||
+            "--'"--'
+        """)
+    if punishment == "2":
+        print("Marcus, you lucky bastard. %s just gave you a pass to live another day.") % name
+elif exercise == "3":
+    print ("""
+     /^------^/
+     V  o o  V
+      |  Y  |
+       \ Q /
+       """)
 else:
-    print("Not available now.")
+    print("Not a valid option. Go back to school you moron.")
